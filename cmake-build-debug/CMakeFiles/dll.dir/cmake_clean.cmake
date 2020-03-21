4fe3d51f1ba7006640664c994cd4c90e1a231e72
孙烨 <17373259@buaa.edu.cn>
2020-03-21 15:27:15 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/dll.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dll.dir/Line.cpp.obj"
+  "libdll.dll"
+  "libdll.dll.a"
+  "libdll.dll.manifest"
+  "libdll.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
